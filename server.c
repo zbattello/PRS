f6dfb4b93d06fb3302cff8f74bdbcfe851fb015b
Enzo Zattarin <62133196+EnzoZatt@users.noreply.github.com>
2022-12-07 23:38:34 +0100

    Update server.c

--- server.c
+++ server.c
@@ -4,6 +4,7 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <string.h>
+#include <sys/time.h>
 
 // Taille enoctet des segments
 #define RCVSIZE 1024
@@ -150,17 +151,29 @@ int main (int argc, char *argv[])
         {
             close(public_socket); // On ferme la socket publique
 
+            struct timeval tv;
+            gettimeofday(&tv, NULL);
+            unsigned long begin = 1000000 * tv.tv_sec + tv.tv_usec;
+
             // Buffer de reception du client
             char msgBufferClient[RCVSIZE];
+
+            // Nom du fichier a envoyer
+            char file_name[RCVSIZE];
 
             // Attente de reception du nom du fichier demande
             recvfrom(client_socket, msgBufferClient, RCVSIZE, 0, (struct sockaddr *)&client_adress, &alen);
 
-            printf("Demande recu du fichier %s.\n", msgBufferClient);
+            // Enregistrement du nom du fichier dans file_name
+            memcpy(file_name, msgBufferClient, RCVSIZE);
+
+            printf("~~~~~~~~~~~~~~~~~~\n");
+
+            printf("Demande recu du fichier '%s'.\n", file_name);
 
             // Overture du fichier a envoyer
             FILE* file = NULL;
-            file = fopen(msgBufferClient, "rb"); //Ouverture binaire
+            file = fopen(file_name, "rb"); //Ouverture binaire
 
             if (file == NULL)
             {
@@ -186,6 +199,7 @@ int main (int argc, char *argv[])
             int segment_ack = 0;
 
             int size = 1;
+            int file_size = 0;
             int win_size;
 
             // Liste des numeros de segment
@@ -199,23 +213,25 @@ int main (int argc, char *argv[])
             // Tant qu'il reste des octets a lire dans le fichier
             while (size > 0)
             {
-                printf("\t\tNombre d'ACK ok : %d.\n", segment_ack);
+                //printf("\t\tNombre d'ACK ok : %d.\n", segment_ack);
 
                 number_window++;
 
-                printf("\tGeneration de la fenetre %d.\n", number_window);
+                //printf("\tGeneration de la fenetre %d.\n", number_window);
                 // Generation de la liste des trames a envoyer (la fenetre)
                 for (k = 0; k < WINSIZE; k++)
                 {
                     // Incrementation du numero de segment               
                     number_segment++;
 
-                    printf("\t\tCreation du segment %d.\n", number_segment);
+                    //printf("\t\tCreation du segment %d.\n", number_segment);
 
                     // Lecture de (RCVSIZE-6) octets du fichier
                     size = fread(fileBuffer, 1, RCVSIZE - 6, file);
 
-                    printf("\t\t\tLecture de %d octets.\n", size);
+                    file_size += size;
+
+                    //printf("\t\t\tLecture de %d octets.\n", size);
 
                     tab_segment[k] = number_segment;
                     tab_size[k] = size;
@@ -223,7 +239,7 @@ int main (int argc, char *argv[])
                     // Si tout les octets ont ete lu
                     if (size == 0)
                     {
-                        printf("\t\t\tPlus d'octet a lire. Fin de la generation de la fenetre.\n");
+                        //printf("\t\t\tPlus d'octet a lire. Fin de la generation de la fenetre.\n");
                         // On sort de la boucle
                         break;
                     }
@@ -236,27 +252,27 @@ int main (int argc, char *argv[])
                 }
 
                 if (tab_size[0] == 0) {
-                    printf("\t\tLa nouvelle fenetre est vide de base. Plus rien a envoyer.\n");
+                    //printf("\t\tLa nouvelle fenetre est vide de base. Plus rien a envoyer.\n");
                     break;
                 }
 
                 // La taille de la fenetre
                 int win_size = k;
 
-                printf("\t\tTaille de la fenetre %d: %d.\n", number_window, win_size);
+                //printf("\t\tTaille de la fenetre %d: %d.\n", number_window, win_size);
 
                 // Tant que le plus grand numero d'ACK recu ne
                 // corespond pas au dernier segment de la fenetre
                 do {
-                    printf("\t\tNombre d'ACK ok : %d.\n", segment_ack);
+                    //printf("\t\tNombre d'ACK ok : %d.\n", segment_ack);
 
-                    printf("\tTransmission de la fenetre %d.\n", number_window);
+                    //printf("\tTransmission de la fenetre %d.\n", number_window);
 
                     // Envoie des trames de la fenetre
                     // a partir du dernier ACK recu
                     for (int i = segment_ack % WINSIZE; i < win_size; i++)
                     {
-                        printf("\t\tEnvoie du segment %d.\n", tab_segment[i]);
+                        //printf("\t\tEnvoie du segment %d.\n", tab_segment[i]);
 
                         // Envoie de la trame
                         sendto(client_socket, window[i], tab_size[i] + 6, 0, (struct sockaddr*)&client_adress, alen);
@@ -265,9 +281,9 @@ int main (int argc, char *argv[])
                         segment_send = (tab_segment[i] > segment_send) ? tab_segment[i] : segment_send;
                     }
 
-                    printf("\tNombre de segment envoie : %d.\n", segment_send);
+                    //printf("\tNombre de segment envoie : %d.\n", segment_send);
 
-                    printf("\tReception des ACK de la fenetre %d.\n", number_window);
+                    //printf("\tReception des ACK de la fenetre %d.\n", number_window);
 
                     // Reception des ACK
                     do {
@@ -295,12 +311,12 @@ int main (int argc, char *argv[])
                             }
                             number_ack = atoi(str_ack);
 
-                            printf("\t\tReception de l'aquitement %d.\n", number_ack);
+                            //printf("\t\tReception de l'aquitement %d.\n", number_ack);
 
                             // Si le numero d'ACK est le plus grand
                             if (number_ack > segment_ack)
                             {
-                                printf("\t\t\tMise a jour de nombre d'ACK ok.\n");
+                                //printf("\t\t\tMise a jour de nombre d'ACK ok.\n");
 
                                 // Alors mise-a-jour du nombre d'ACK recu
                                 segment_ack = number_ack;
@@ -310,7 +326,7 @@ int main (int argc, char *argv[])
                         // Si on a rien recu avant le timeout
                         else
                         {
-                            printf("\t\tAucun aquitement recu. Recommencement de la transmission.\n");
+                            //printf("\t\tAucun aquitement recu. Recommencement de la transmission.\n");
                             break;
                         }
 
@@ -319,7 +335,7 @@ int main (int argc, char *argv[])
 
                 } while (segment_ack < segment_send);
 
-                printf("\tFin de la transmission de la fenetre %d.\n", number_window);
+                //printf("\tFin de la transmission de la fenetre %d.\n", number_window);
             }
 
             // Le transfert de fichier est termine
@@ -327,9 +343,22 @@ int main (int argc, char *argv[])
             char endMsg[3] = "FIN";
             sendto(client_socket, endMsg, 3, 0,(struct sockaddr*)&client_adress, sizeof(client_adress));
 
-            printf("Fin du transfert");
+            printf("Fin du transfert de '%s'.\n", file_name);
 
             fclose(file);
+
+            gettimeofday(&tv, NULL);
+            unsigned long end = 1000000 * tv.tv_sec + tv.tv_usec;
+
+            float exec_time = (float)(end - begin) / 1000000;
+            float debit = (float)(file_size) * 0.000001 / exec_time;
+
+            printf("Taille du fichier : %f mega-octets\n", (float)(file_size) * 0.000001);
+            printf("Temps d'execution : %f secondes\n", exec_time);
+            printf("Debit : %f mo/s\n", debit);
+
+            printf("~~~~~~~~~~~~~~~~~~\n");
+
             return 0;
         }
     }
